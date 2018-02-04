904cdc891ae5b31c72474e3448b7e09119ac633e
福治 菜摘美 <natsumi.fukuji@recochoku.co.jp>
2018-02-04 13:17:02 +0900

    [modify] ラジオ画面のサンプルデータの追加

--- src/js/components/Radio/Contents.js
+++ src/js/components/Radio/Contents.js
@@ -5,11 +5,12 @@ import { colors } from 'material-ui';
 
 const homeContents = [
   { message: 'この曲好き！', image: 'http://i.scdn.co/image/d8ad6363ac1c6912369fbeb3b6efff419beec4d1', id: 'spotify:album:5ht7ItJgpBH7W6vJ5BqpPr' },
-  { message: 'この曲好き！', image: 'http://i.scdn.co/image/d8ad6363ac1c6912369fbeb3b6efff419beec4d1', id: 'spotify:album:5ht7ItJgpBH7W6vJ5BqpPr' },
-  { message: 'この曲好き！', image: 'http://i.scdn.co/image/d8ad6363ac1c6912369fbeb3b6efff419beec4d1', id: 'spotify:album:5ht7ItJgpBH7W6vJ5BqpPr' },
-  { message: 'この曲好き！', image: 'http://i.scdn.co/image/d8ad6363ac1c6912369fbeb3b6efff419beec4d1', id: 'spotify:album:5ht7ItJgpBH7W6vJ5BqpPr' },
-  { message: 'この曲好き！', image: 'http://i.scdn.co/image/d8ad6363ac1c6912369fbeb3b6efff419beec4d1', id: 'spotify:album:5ht7ItJgpBH7W6vJ5BqpPr' },
-  { message: 'この曲好き！', image: 'http://i.scdn.co/image/d8ad6363ac1c6912369fbeb3b6efff419beec4d1', id: 'spotify:album:5ht7ItJgpBH7W6vJ5BqpPr' },
+  { message: '通学路で聴いてます！', image: 'https://i.scdn.co/image/c5864968f30c3c7298a403b0d2c8d652a0d7c039', id: 'spotify:album:5bMtF0QUf1rKcoqOvpBEBE' },
+  { message: 'かっこいいなあ', image: 'https://i.scdn.co/image/f2155133a2d08d50f2d9e84bf3fc2e1d28210c82', id: 'spotify:album:2QBJu1rJWSPtTiWxwIY2H7' },
+  { message: 'これ、おすすめ！', image: 'https://i.scdn.co/image/7c3ec33d478f5f517eeb5339c2f75f150e4d601e', id: 'spotify:album:4OHNH3sDzIxnmUADXzv2kT' },
+  { message: '永遠の名曲だよな。', image: 'https://i.scdn.co/image/7c9d2b8dab447da632caccd259bae20f605e4b02', id: 'spotify:album:3a5aKQqcG1rTAUcs3qYFda' },
+  { message: '最近、ハマってる', image: 'https://i.scdn.co/image/ac1c30bff38ad98165671c933b6279a6cd6885e1', id: 'spotify:album:64ZJtdPNj5HqXCsT1MHokY' },
+  { message: '落ち着きたい時によくかける曲です', image: 'https://i.scdn.co/image/21d4f9be39f28ba3295efa2a597353cb812dec81', id: 'spotify:album:5TgzfNr91dbdvZWC0dRldi' },
 ];
 
 
@@ -26,7 +27,7 @@ export default class Contents extends React.Component {
       case 'play':
         this.spotify.play(payload);
         break;
-    
+
       default:
         break;
     }
@@ -71,3 +72,9 @@ export default class Contents extends React.Component {
     );
   }
 }
+https://open.spotify.com/track/4dFXu3u2hTk2cfVjK2IGRX
+https://open.spotify.com/track/04JL2liXXV9B9coeGuUsPw
+https://open.spotify.com/track/5TOSXYdmL0g0cjgS5EmERP
+https://open.spotify.com/track/2dQ4alICWrgs9gyYUOWlEg
+https://open.spotify.com/track/1zNNR1sQvk2W55jqweXFiX
+https://open.spotify.com/track/4HFqtbl5D5gJhtjnGRhGgY
